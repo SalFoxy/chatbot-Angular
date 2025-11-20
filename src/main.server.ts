54773c1afddcaa47ffb89e0e184b339ff7569fe5
s.scalisi <s.scalisi@reply.it>
2025-11-20 11:39:23 +0100

    Prima dell'update Angular

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { ChatComponent } from './app/chat/chat.component';
 import { appConfig } from './app/app.config';
 
 export default function bootstrap() {
-  return bootstrapApplication(ChatComponent, appConfig);
+  return bootstrapApplication(ChatComponent, appConfig, context);
 }
